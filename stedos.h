8d4c8b6e7b67c11e74363a9f84bfc629ce836b73
Steddy Smit <jacob.smit@zepler.net>
2015-04-08 01:04:47 +0100

    Put all of the parts into the header file.  Still needs sorting.

--- stedos.h
+++ stedos.h
@@ -2,6 +2,296 @@
 #include <iostream>
 using namespace std;
 
+
+#include <stdint.h>
+//#include <avr/io.h>
+//#include <avr/interrupt.h>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+class DebugIface
+{
+public:
+    virtual void push() = 0;
+    virtual void pop() = 0;
+};
+
+class NoDebug : DebugIface
+{
+public:
+  void push() {};
+  void pop() {};
+};
+
+class Debug : DebugIface
+{
+public:
+  void push() { ++size; if (size > max_size) max_size = size; }
+  void pop() { --size; }
+  Debug() : size(0), max_size(0) {};
+
+  uint16_t size;
+  uint16_t max_size;
+
+};
+
+template <typename T, int SIZE, typename DEBUG=NoDebug>
+class FIFO
+{
+  static_assert(SIZE > 0, "");
+  static_assert(SIZE <= 256, "");
+
+  const uint8_t MASK = (SIZE - 1);
+
+  T array[SIZE];
+  uint8_t head;
+  uint8_t tail;
+  
+
+public:
+  void        push(const T& v) { debug.push(); inc(head);  array[head] = v; }
+  const T&    pop()            { debug.pop();  inc(tail);  return array[tail]; }
+  const T&    peek()           { uint8_t temp = tail; inc(temp); return array[temp]; }
+  FIFO() : head(0), tail(0) {};
+
+  DEBUG debug;
+
+protected:
+  void inc(uint8_t& v)
+  {
+    v += 1;
+
+    if((SIZE == 2)  || (SIZE == 4)  || (SIZE == 8)   || (SIZE == 16)   ||
+       (SIZE == 32) || (SIZE == 64) || (SIZE == 128) || (SIZE == 256))
+    {
+      v &= MASK; // use MASK to limit index (optimisation if SIZE is a power of 2)
+    }
+    else
+    {
+      if(v == SIZE)
+      {
+        v = 0;  // use SIZE to limit index
+      }
+    }
+  }
+
+};
+
+int main(void)
+{
+  const int BUF_SZ = 4;
+  FIFO<char, BUF_SZ, Debug> buffer;
+
+  const char hello[] = "helloooab";
+  for(int j=0; j<BUF_SZ; ++j)
+  {
+    char letter = 'a' + (j);
+    //cout << "push : " << j << " " << letter << endl;
+    buffer.push(letter);
+  }
+
+  // Read item from buffer
+  for(int i=0; i<BUF_SZ; ++i)
+  {
+    //char v = buffer.peek();
+    char w = buffer.pop();
+    buffer.push(w);
+    //PORTB = w;
+    //cout << "pop  : " << i << " " << v << w << endl;
+  }
+
+  cout << "Debug : " << buffer.debug.max_size << endl;
+
+  return 0;
+}
+
+
+-------------------------------------------------------------------------------------------
+
+
+#include <stdint.h>
+#include <avr/io.h>
+#define NULL (0)
+
+/* Define the list of port addresses
+   This is an indexable list of addresses, that is
+   filled in based on the hardware ports that
+   are available on the AVR.
+   If the relevant #define exist, then the table
+   entry is populated with the appropriate address.
+   If the relevant #define does not exist, then
+   the entry is NULL.
+*/
+volatile uint8_t* const port_table[] = 
+{
+  #ifdef PORTA
+    &PORTA,
+  #else
+    NULL,
+  #endif
+  #ifdef PORTB
+    &PORTB,
+  #else
+    NULL,  
+  #endif
+  #ifdef PORTC
+    &PORTC,
+  #else
+    NULL,
+  #endif
+  #ifdef PORTD
+    &PORTD,
+  #else
+    NULL,
+  #endif
+  #ifdef PORTE
+    &PORTE,
+  #else
+    NULL,
+  #endif
+  #ifdef PORTF
+    &PORTF,
+  #else
+    NULL,
+  #endif
+  #ifdef PORTG
+    &PORTG,
+  #else
+    NULL,
+  #endif
+};
+
+/* Here we define the class that controls access to the
+   port.  It takes a template argument as the index to
+   the port address.  */
+template<int PORT_ID>
+struct port {
+  volatile uint8_t* const address = port_table[PORT_ID];
+  uint8_t operator |= (uint8_t v) { return *address |= v; }
+  uint8_t operator &= (uint8_t v) { return *address &= v; }
+  uint8_t read() const { return *address; }
+  void write(uint8_t v) { *address = v; }
+};
+
+/* Finally we define our own class instances to access
+   each port.  These will only be defined if the relevant 
+   #define exists.  This is what should be used to access
+   the ports.  */
+
+#ifdef PORTA
+  using port_a = port<0>;
+#endif
+#ifdef PORTB
+  using port_b = port<1>;
+#endif
+#ifdef PORTC
+  using port_c = port<2>;
+#endif
+#ifdef PORTD
+  using port_d = port<3>;
+#endif
+#ifdef PORTE
+  using port_e = port<4>;
+#endif
+#ifdef PORTF
+  using port_f = port<5>;
+#endif
+#ifdef PORTG
+  using port_g = port<6>;
+#endif
+
+
+/* This device class allows us to access a subset of the
+   bits of a port.  It is arguably more useful than port as
+   very rarely, can you talk to an entire port at once.
+
+
+*/
+/* This class template allows us to talk to a group of pins */
+namespace cpp11 {
+  template<bool B, class T = void>
+  struct enable_if {};
+ 
+  template<class T>
+  struct enable_if<true, T> { typedef T type; };
+}
+
+
+template <typename PORT, int OFFSET, int LENGTH=1>
+struct device
+{
+  static_assert(LENGTH >  0, "");
+  static_assert(LENGTH <= 8, "");
+  static_assert(OFFSET <  8, "");
+
+  const uint8_t MASK = ((1 << LENGTH) - 1) << OFFSET;
+  PORT port;
+
+  template<int FOO=LENGTH>
+  typename cpp11::enable_if<(FOO > 1)>::type
+  set(uint8_t v) { uint8_t temp = port.read(); temp &= ~MASK; temp |= v; port.write(temp); }
+
+  template<int FOO=LENGTH>
+  typename cpp11::enable_if<(FOO == 1)>::type
+  set(uint8_t v, int = 0) { if(v) set(); else clr(); }
+
+  /*
+  void set(uint8_t v) {
+    if (LENGTH == 1) {
+      if(v) set(); else clr();
+    } else {
+      uint8_t temp = port.read(); temp &= ~MASK; temp |= v; port.write(temp);
+    }
+  }
+*/
+  void set()          { port |=  MASK; }
+  void clr()          { port &= ~MASK; }
+  void tgl()          { port ^=  MASK; }
+  uint
+};
+
+template <typename PORT, int OFFSET, int LENGTH=1>
+struct read_only_device
+{
+  static_assert(LENGTH >  0, "");
+  static_assert(LENGTH <= 8, "");
+  static_assert(OFFSET <  8, "");
+
+  const uint8_t MASK = ((1 << LENGTH) - 1) << OFFSET;
+  PORT port;
+};
+
+/*
+template <typename PORT, int OFFSET>
+struct device<PORT, OFFSET, 1>
+{
+  PORT port;
+  void set() { port |=  (1 << OFFSET); }
+  void clr() { port &= ~(1 << OFFSET); }
+  void tgl() { port ^=  (1 << OFFSET); }
+};
+*/
+int main(void)
+{
+  device<port_b, 1> led;
+
+template <typename PORT, int OFFSET, int LENGTH=1>
+using read_only_device = device<PORT, OFFSET, LENGTH, read_only>;
+
+  read_only_device<port_b, 1> led;
+  read_only_device<port_b, 1> led;
+
+  device<port_b, 2, 2> blah;
+  led.set((uint8_t) 1);
+  blah.set(2);
+
+  return 0;
+}
+
+
 /* Array */
 template <class T, int maxItems>
 class Array
