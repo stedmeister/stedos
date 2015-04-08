ef8aae4cd92ff266cce1aa82a390d420df73b420
Steddy Smit <jacob.smit@zepler.net>
2015-04-08 23:12:37 +0100

    Started to bring all of the bits together

--- stedos.h
+++ stedos.h
@@ -1,4 +1,166 @@
-/* This class is a funky class */
+/*
+ * StedOS
+ *
+ * A simple AVR 'operating system'
+ *
+ * (c) stedmeister
+ *
+ * Licesnse TBD
+ */
+
+ namespace stedos
+ {
+    /**********************************************************
+     *
+     * Useful storage classes
+     *
+     **********************************************************/
+
+    /**
+     * FIFO - Items are pushed onto the end
+     *        and read from the front
+     *
+     * Template Parameters:
+     *      T - class of the data that needs to be stored
+     *   SIZE - max items (currently limited to a max size of 256)
+     */
+    template <typename T, int SIZE>
+    class FIFO
+    {
+        /* check the parameters */
+        static_assert(SIZE > 0, "");
+        static_assert(SIZE <= 256, "");
+
+        /* MASK is used if SIZE if a multiple of 2
+           it can be used to limit the index by an &=
+           operation.  */
+        const uint8_t MASK = (SIZE - 1);
+
+
+    public:
+        /** Adds an item to the end of the queue */
+        void        push(const T& v) { inc(head);  array[head] = v; }
+
+        /** Removes an item from the front of the queue */
+        const T&    pop()            { inc(tail);  return array[tail]; }
+
+        /** Looks at the next item to be popped */
+        const T&    peek()           { uint8_t temp = tail; inc(temp); return array[temp]; }
+
+        /** Checks to see if the buffer is empty */
+        uint8_t     isEmpty()        { return head == tail; }
+
+        FIFO() : head(0), tail(0) {};
+
+
+    private:
+        T array[SIZE];    /* Allocate the memory to store the items */
+        uint8_t head;     /* Keep an index to the head of the circular buffer */
+        uint8_t tail;     /* Keep an index to the tail of the circular buffer */
+      
+        /** This function increments the indices.
+            It increments the number and then limits it to SIZE.
+            If SIZE is 256, then index rollover is used
+            If SIZE is a multiple of 2, then MASK can be used
+         */
+        void inc(uint8_t& v)
+        {
+            v += 1;
+
+            if (SIZE == 256)
+            {
+                /* Nothing to do */
+            }
+            else if ((SIZE == 2)  || (SIZE == 4)  || (SIZE == 8)   || (SIZE == 16)   ||
+                     (SIZE == 32) || (SIZE == 64) || (SIZE == 128))
+            {
+                v &= MASK; /* use MASK to limit index (optimisation if SIZE is a power of 2) */
+            }
+            else
+            {
+                if(v == SIZE)
+                {
+                    v = 0;  // use SIZE to limit index
+                }
+            }
+        }
+
+    };
+
+
+
+    /**********************************************************
+     *
+     * Event processing
+     *
+     **********************************************************/
+
+
+    /* Define a function prototype to use for structures */
+    typedef void (*event_func_t) (void* data);
+
+    /* Define a struct used to store a function pointer
+       and the user data associated with it
+    */
+    struct event_t
+    {
+        event_func_t func;
+        void*        data;
+    };
+
+    template <int SIZE>
+    class EventProcessor
+    {
+    public:
+        /** Adds an event to the queue */
+        void queueEvent(event_func_t func, void* data) { events.push({ func, data}); }
+        void queueEvent(const event_t& event) { events.push(event); }
+        void process()
+        {
+            while(events.isEmpty() == false)
+            {
+                event_t event = events.pop();
+                event.func(event.data);
+            }
+        }
+
+    private:
+        FIFO<event_t, SIZE> events;
+    };
+
+}
+#if 0
+class ProcessInterface
+{
+public:
+    virtual void queueEvent(callbackFunction f, void* data);
+};
+
+typedef struct
+{
+    callbackFunction callback;
+    void* data;
+} QueueData_t;
+
+template <int maxEvents>
+class SimpleProcess : public ProcessInterface
+{
+public:
+    virtual void queueEvent(callbackFunction f, void* data);
+
+private:
+    QueueData_t eventQueue[maxEvents];
+};
+
+template <class T>
+class Process : public T
+{
+};
+
+
+ }
+
+
 #include <iostream>
 using namespace std;
 
@@ -37,49 +199,47 @@ public:
   uint16_t max_size;
 
 };
-
 template <typename T, int SIZE, typename DEBUG=NoDebug>
-class FIFO
-{
-  static_assert(SIZE > 0, "");
-  static_assert(SIZE <= 256, "");
-
-  const uint8_t MASK = (SIZE - 1);
-
-  T array[SIZE];
-  uint8_t head;
-  uint8_t tail;
-  
-
-public:
-  void        push(const T& v) { debug.push(); inc(head);  array[head] = v; }
-  const T&    pop()            { debug.pop();  inc(tail);  return array[tail]; }
-  const T&    peek()           { uint8_t temp = tail; inc(temp); return array[temp]; }
-  FIFO() : head(0), tail(0) {};
-
-  DEBUG debug;
-
-protected:
-  void inc(uint8_t& v)
-  {
-    v += 1;
-
-    if((SIZE == 2)  || (SIZE == 4)  || (SIZE == 8)   || (SIZE == 16)   ||
-       (SIZE == 32) || (SIZE == 64) || (SIZE == 128) || (SIZE == 256))
+    class FIFO
     {
-      v &= MASK; // use MASK to limit index (optimisation if SIZE is a power of 2)
-    }
-    else
-    {
-      if(v == SIZE)
+      static_assert(SIZE > 0, "");
+      static_assert(SIZE <= 256, "");
+
+      const uint8_t MASK = (SIZE - 1);
+
+      T array[SIZE];
+      uint8_t head;
+      uint8_t tail;
+      
+
+    public:
+      void        push(const T& v) { debug.push(); inc(head);  array[head] = v; }
+      const T&    pop()            { debug.pop();  inc(tail);  return array[tail]; }
+      const T&    peek()           { uint8_t temp = tail; inc(temp); return array[temp]; }
+      FIFO() : head(0), tail(0) {};
+
+      DEBUG debug;
+
+    protected:
+      void inc(uint8_t& v)
       {
-        v = 0;  // use SIZE to limit index
+        v += 1;
+
+        if((SIZE == 2)  || (SIZE == 4)  || (SIZE == 8)   || (SIZE == 16)   ||
+           (SIZE == 32) || (SIZE == 64) || (SIZE == 128) || (SIZE == 256))
+        {
+          v &= MASK; // use MASK to limit index (optimisation if SIZE is a power of 2)
+        }
+        else
+        {
+          if(v == SIZE)
+          {
+            v = 0;  // use SIZE to limit index
+          }
+        }
       }
-    }
-  }
 
-};
-
+    };
 int main(void)
 {
   const int BUF_SZ = 4;
@@ -297,91 +457,61 @@ template <class T, int maxItems>
 class Array
 {
 public:
-	Array() : currentSize(0) {};
-	T& operator[](int idx) { return this->items[idx]; };
-	void pushBack(T& item) { items[currentSize++] = item; };
+    Array() : currentSize(0) {};
+    T& operator[](int idx) { return this->items[idx]; };
+    void pushBack(T& item) { items[currentSize++] = item; };
 
 private:
-	uint16_t currentSize;
-	T items[maxItems];
+    uint16_t currentSize;
+    T items[maxItems];
 };
 
 template <class T, int maxItems>
 class LinkedList
 {
 public:
-	LinkedList() : first(NULL), last(NULL) {};
-	void pushBack(T& item)
-	{
-		ItemData* item = addItem(item);
-		item->next = NULL;
-		this->last->next = item;
-		this->last = item;
+    LinkedList() : first(NULL), last(NULL) {};
+    void pushBack(T& item)
+    {
+        ItemData* item = addItem(item);
+        item->next = NULL;
+        this->last->next = item;
+        this->last = item;
 
-		if(this->first == NULL) this->first = item;
-	};
-	void pushFront(T& item)
-	{
-		ItemData* item = addItem(item);
-		item->next = this->first;
-		this->first = item;
+        if(this->first == NULL) this->first = item;
+    };
+    void pushFront(T& item)
+    {
+        ItemData* item = addItem(item);
+        item->next = this->first;
+        this->first = item;
 
-		if(this->last == NULL) this->last = item;
-	};
+        if(this->last == NULL) this->last = item;
+    };
 private:
-	class ItemData
-	{
-		uint8_t inUse;
-		T data;
-		ItemData* next;
-	} items[maxItems];
+    class ItemData
+    {
+        uint8_t inUse;
+        T data;
+        ItemData* next;
+    } items[maxItems];
 
-	ItemData* first;
-	ItemData* last;
+    ItemData* first;
+    ItemData* last;
 
-	ItemData* addItem(T& item)
-	{
-		for(uint16_t idx=0; idx<maxItems; idx+=1)
-		{
-			if (items[idx].inUse == 0)
-			{
-				items[idx].data = item;
-				items[idx].inUse = 1;
-				return &items[idx];
-			}
-		}
-		return NULL;
-	}
-};
-
-/* Callback definitions */
-typedef void (*callbackFunction) (void* data);
-
-class ProcessInterface
-{
-public:
-	virtual void queueEvent(callbackFunction f, void* data);
-};
-
-typedef struct
-{
-	callbackFunction callback;
-	void* data;
-} QueueData_t;
-
-template <int maxEvents>
-class SimpleProcess : public ProcessInterface
-{
-public:
-	virtual void queueEvent(callbackFunction f, void* data);
-
-private:
-	QueueData_t eventQueue[maxEvents];
-};
-
-template <class T>
-class Process : public T
-{
+    ItemData* addItem(T& item)
+    {
+        for(uint16_t idx=0; idx<maxItems; idx+=1)
+        {
+            if (items[idx].inUse == 0)
+            {
+                items[idx].data = item;
+                items[idx].inUse = 1;
+                return &items[idx];
+            }
+        }
+        return NULL;
+    }
 };
 
 template <int maxEvents>
@@ -396,9 +526,9 @@ void SimpleProcess<maxEvents>::queueEvent(callbackFunction f, void* data)
 class TimerInterface
 {
 public:
-	virtual void    increment(void) = 0;
-	virtual uint8_t addEvent(uint16_t timeout, callbackFunction f, void* userData) = 0;
-	virtual void    callback(callbackFunction f, void* userData) = 0;
+    virtual void    increment(void) = 0;
+    virtual uint8_t addEvent(uint16_t timeout, callbackFunction f, void* userData) = 0;
+    virtual void    callback(callbackFunction f, void* userData) = 0;
 };
 
 /* This class maintains a simple list.
@@ -406,65 +536,67 @@ public:
 */
 typedef struct
 {
-	uint16_t ticks;
-	callbackFunction callback;
-	void* userData;
+    uint16_t ticks;
+    callbackFunction callback;
+    void* userData;
 } TimerQueue_t;
 
 template<int queueLength>
 class SimpleTimerImplementation : public TimerInterface
 {
 public:
-	virtual void increment(void);
-	virtual uint8_t addEvent(uint16_t timeout, callbackFunction f, void* u);
+    virtual void increment(void);
+    virtual uint8_t addEvent(uint16_t timeout, callbackFunction f, void* u);
 
 private:
-	TimerQueue_t queue[queueLength];
+    TimerQueue_t queue[queueLength];
 };
 
 template<class T>
 class Timer : public T
 {
-	virtual void callback(callbackFunction f, void* userData) { f(userData); };
+    virtual void callback(callbackFunction f, void* userData) { f(userData); };
 };
 
 template<int queueLength>
 void SimpleTimerImplementation<queueLength>::increment(void)
 {
-	//cout << "SimpleTimerImplementation::increment" << endl;
-	/* Go through all of the queue items.
-	   Decrement them and call the callback if necessary.  */
-	for (uint8_t idx=0; idx<queueLength; idx+=1)
-	{
-		if (this->queue[idx].ticks > 0)
-		{
-			this->queue[idx].ticks -= 1;
+    //cout << "SimpleTimerImplementation::increment" << endl;
+    /* Go through all of the queue items.
+       Decrement them and call the callback if necessary.  */
+    for (uint8_t idx=0; idx<queueLength; idx+=1)
+    {
+        if (this->queue[idx].ticks > 0)
+        {
+            this->queue[idx].ticks -= 1;
 
-			if (this->queue[idx].ticks == 0)
-			{
-				this->callback(this->queue[idx].callback, this->queue[idx].userData);
-				//cout << "callback " << idx << endl;
-			}
-		}
-	}
+            if (this->queue[idx].ticks == 0)
+            {
+                this->callback(this->queue[idx].callback, this->queue[idx].userData);
+                //cout << "callback " << idx << endl;
+            }
+        }
+    }
 }
 
 template<int queueLength>
 uint8_t SimpleTimerImplementation<queueLength>::addEvent(uint16_t timeout, callbackFunction f, void* userData)
 {
-	//cout << "SimpleTimerImplementation::addEvent" << endl;
+    //cout << "SimpleTimerImplementation::addEvent" << endl;
 
-	/* Go through the queue list, find a spare space for this item */
-	for (uint8_t idx=0; idx<queueLength; idx+=1)
-	{
-		if (this->queue[idx].ticks == 0)
-		{
-			this->queue[idx].ticks = timeout;
-			this->queue[idx].callback = f;
-			this->queue[idx].userData = userData;
-			return idx;
-		}
-	}
+    /* Go through the queue list, find a spare space for this item */
+    for (uint8_t idx=0; idx<queueLength; idx+=1)
+    {
+        if (this->queue[idx].ticks == 0)
+        {
+            this->queue[idx].ticks = timeout;
+            this->queue[idx].callback = f;
+            this->queue[idx].userData = userData;
+            return idx;
+        }
+    }
 
-	return 0xff; /* Invalid Handle */
+    return 0xff; /* Invalid Handle */
 }
+
+#endif
